8cf4cf79a352ed9bbb50359e0c2aaa829ecea441
simshadows <simshadows@users.noreply.github.com>
2017-05-05 11:14:02 +1000

    Update testdpcutipy.py
    This test file should do roughly the same thing as the last test file.
    
    TODO: Make it fuzz all registers, and demonstrate the dpcutil API use rather than put_single_register and get_single_register. (I can't really do that atm since I don't have a physical device to test it on, even if the code's simple enough.)

--- testdpcutipy.py
+++ testdpcutipy.py
@@ -2,7 +2,7 @@
 testdpcutipy.py
 Author: simshadows
 
-A very simple test file that just writes some data in, then reads and prints it back.
+A very simple test file that just writes to a register, then reads and prints it back.
 
 This also provides a simple demonstration of the operation of the dpcutipy library.
 """
@@ -14,31 +14,19 @@ import traceback
 
 from dpcutipy import dpcutipy
 
+reg_id = random.randint(0,64)
+reg_val = random.randint(0,256)
+
 dpcutipy.DvmgStartConfigureDevices() # Comment back in if needed.
 dev_id = dpcutipy.DvmgGetDefaultDev()
 dev_name = dpcutipy.DvmgGetDevName(dev_id)
 
 print("Default device: " + dev_name)
 
-# Test code
+dpcutipy.put_single_register(reg_id, reg_val, dev_name)
+print("REGISTER {} WRITE: {}".format(str(reg_id), str(reg_val)))
 
-# writes = [
-# 	(0, 21),
-# 	(1, 42),
-# 	(2, 37),
-# 	(3, 44),
-# 	(4, 58),
-# 	(10, 194),
-# ]
-
-writes = [(64, random.randint(0,255))]
-
-for (reg, val) in writes:
-	dpcutipy.put_single_register(reg, val, dev_name)
-	print("REGISTER {} WRITE: {}".format(str(reg), str(val)))
-
-for (reg, val) in [(x,0) for x in range(0x100)]: # writes:
-	ret = dpcutipy.get_single_register(reg, dev_name)
-	print("REGISTER {} READ: {}".format(str(reg), str(ret)))
+ret = dpcutipy.get_single_register(reg_id, dev_name)
+print("REGISTER {} READ: {}".format(str(reg_id), str(ret)))
 
 print("DONE!")
